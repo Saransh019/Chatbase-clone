4726e89066a81f4d3ff56f9c62a1566e86c04f92
Saransh019 <b20132@students.iitmandi.ac.in>
2023-10-19 11:42:04 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,101 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Chatbot.module.css";
+import { useState } from "react";
+import { Suggestions } from "../../Constants";
 
-export default function Home() {
+
+export default function Chatbot() {
+  const [msgs, setMsgs] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [chat, setChat] = useState(false);
+
+  function addMsg(text) {
+    const x = <div className={styles.sentmsg}>{text}</div>;
+    const y = <div className={styles.recmsg}>{text}</div>;
+    const msgg = [...msgs, x, y];
+    setMsgs(msgg);
+  }
+
+  const handleClick = () => {
+    if(inputValue!="")
+      addMsg(inputValue);
+    setInputValue("");
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const reload = () => {
+    const msgg = [];
+    setMsgs(msgg);
+  };
+  const close = () => {
+    setChat(false);
+  };
+
+  const handleChatBot = () => {
+    setChat(~chat);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Chatbase clone</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.container}>
+          {chat ? (
+            <div className={styles.wrapper}>
+              <div className={styles.wrapperTop}>
+                <div className={styles.top}>
+                  <div className={styles.topLeft}>
+                    <img src="logo.png" />
+                    <div className={styles.topHeading}>Chatbase</div>
+                  </div>
+                  <div className={styles.topButtons}>
+                    <img src="reload.png" onClick={reload}></img>
+                    <img src="close.png" onClick={close}></img>
+                  </div>
+                </div>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <div className={styles.messageBoard}>
+                  {msgs.map((ele) => ele)}
+                </div>
+              </div>
+              <div className={styles.wrapperBottom}>
+                <div className={styles.suggestions}>
+                  {Suggestions.map((sug) => (
+                    <div
+                      className={styles.suggestion}
+                      onClick={() => addMsg(sug)}
+                    >
+                      {sug}
+                    </div>
+                  ))}
+                </div>
+                <div className={styles.inputBox}>
+                  <input
+                    className={styles.input}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                  ></input>
+                  <img
+                    src="go.png"
+                    onClick={handleClick}
+                    className={styles.go}
+                  />
+                </div>
+              </div>
+            </div>
+          ) : null}
+          <div className={styles.logoo}>
+            <img src={chat ? "down.svg" : "logo.png"} onClick={handleChatBot} />
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
